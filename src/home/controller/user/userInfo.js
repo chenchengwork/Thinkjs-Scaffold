8781731fb70be4349f017b9ccff72e11ff473bef
chencheng <cheng.chen@tianjishuju.com>
2017-08-05 19:09:13 +0800

    second commit

--- src/home/controller/user/userInfo.js
+++ src/home/controller/user/userInfo.js
@@ -10,12 +10,15 @@ const rename = think.promisify(fs.rename, fs);
 module.exports = class extends Base{
 	async indexAction(ctx){
 
-		const file = this.file('filename');
+		console.log(think.config('workers'));
 
-		// 如果上传的是 png 格式的图片文件，则移动到其他目录
-		const filepath = path.join(think.ROOT_PATH, 'www/upload/a.png');
-		think.mkdir(path.dirname(filepath));
-		await rename(file.path, filepath);
+		//文件上传
+		// const file = this.file('filename');
+		//
+		// // 如果上传的是 png 格式的图片文件，则移动到其他目录
+		// const filepath = path.join(think.ROOT_PATH, 'www/upload/a.png');
+		// think.mkdir(path.dirname(filepath));
+		// await rename(file.path, filepath);
 
 		this.body = "this is userInfo";
 	}
